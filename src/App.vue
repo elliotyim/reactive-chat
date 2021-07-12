5282c5425a6af46786147d11fe34ce679beaadfc
Elliot Yim <yimsb02@naver.com>
2021-07-12 23:45:02 +0900

    Initialize a project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <AppBar />
+    <NavigationDrawer />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppBar from "./components/AppBar.vue";
+import NavigationDrawer from "./components/NavigationDrawer.vue";
+
+export default {
+  name: "App",
+  components: { AppBar, NavigationDrawer },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-navigation-drawer v-model="drawer" mini-variant app permanent>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawer",
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Chat", icon: "mdi-message-text" },
+        // { title: "My Account", icon: "mdi-account" },
+        // { title: "Users", icon: "mdi-account-multiple" },
+        // { title: "Settings", icon: "mdi-cog" },
+      ],
+    };
+  },
+  watch: {
+    drawer(val) {
+      console.log(val);
+    },
+  },
+};
+</script>
