50409608711094881b929b6629114fcc935b6685
Elliot Yim <yimsb02@naver.com>
2021-07-13 21:42:23 +0900

    Implement sample chat ui layout

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -2,10 +2,10 @@
   <v-system-bar window dark app>
     <v-icon>mdi-message</v-icon>
     <span>10 unread messages</span>
-    <!-- <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
     <v-icon>mdi-minus</v-icon>
-      <v-icon>mdi-checkbox-blank-outline</v-icon>
-      <v-icon>mdi-close</v-icon> -->
+    <v-icon>mdi-checkbox-blank-outline</v-icon>
+    <v-icon>mdi-close</v-icon>
   </v-system-bar>
 </template>
 
